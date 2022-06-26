8d0ec392e455e916b3b9c19bb1686ebd8265a601
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-26 13:08:35 +0300

    commit tree a7368cce01332928fde1a58efa6698e93e367168

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 /* Your Code Here */
+function createEmployeeRecord(employees){
+    return{
+        firstName: employees[0],
+        familyName: employees[1],
+        title: employees[2],
+        payPerHour:employees[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }; 
+};
+    
+function createEmployeeRecords(arr){
+    return arr.map((records) =>{
+        return  createEmployeeRecord(records)
+    })
+}
+    
+function getHour(dateTime){
+    return parseInt(dateTime.match(/\d{4}$/)[0])
+}
 
+function getDate(dateTime) {
+    return dateTime.match(/\d{4}-\d{2}-\d{2}/)[0]
+}
+
+function createTimeInEvent(timeIn){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: getDate(timeIn),
+        hour: getHour(timeIn)
+    })
+    
+    return this;
+}
+    
+function  createTimeOutEvent(timeOut){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: getDate(timeOut),
+        hour: getHour(timeOut)
+        })
+    return this;
+}
+    
+function hoursWorkedOnDate(dates) {
+    let timeIn = this.timeInEvents.find(event =>
+        event.date == dates)
+    let timeOut = this.timeOutEvents.find(event =>
+        event.date == dates)
+    let totalTime = (timeOut.hour - timeIn.hour) / 100
+    return totalTime;
+}
+
+function wagesEarnedOnDate(dates) {
+    let hours = hoursWorkedOnDate.call(this, dates)
+    return this.payPerHour * hours;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName == firstName)
+}
+
+function calculatePayroll(employeeRecord) {
+    return employeeRecord.reduce((total, employees) => {
+        return total + allWagesFor.call(employees)
+    }, 0)
+}
+    
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
